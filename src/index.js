72a20c89b25d9426a4c10471938389be0feb07e2
mmolinaris <martamolinaris@hotmail.it>
2026-04-11 15:56:33 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GymApp from './GymApp';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<GymApp />);
